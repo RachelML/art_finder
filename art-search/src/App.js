9ce955ce69048c139a876348b5c480272c579d3e
Rachel Long <rachellong@Rachels-MacBook-Pro.local>
2020-01-24 14:19:22 -0500

    build components

---
+++ art-search/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Route, NavLink } from 'react-router-dom';
+import Axios from 'axios'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Main from './components/Main'
+
+function App() {
+  return (
+    <div className="App">
+   
+    </div>
+  );
+}
+
+export default App;
